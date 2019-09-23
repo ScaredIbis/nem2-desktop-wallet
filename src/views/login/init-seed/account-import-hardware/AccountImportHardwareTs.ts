196f395cb5547728bb4d7b405e8315d65dd6a186
Chris Martin <chris@crunchycloud.io>
2019-09-23 21:06:16 +1000

    uncomment trezorConnect function calls

--- src/views/login/init-seed/account-import-hardware/AccountImportHardwareTs.ts
+++ src/views/login/init-seed/account-import-hardware/AccountImportHardwareTs.ts
@@ -54,49 +54,15 @@ export class AccountImportHardwareTs extends Vue {
 
         // TODO: turn it on for real when we have a device
 
+        const publicKeyResult = await trezor.getPublicKey({
+            path: `m/44'/43'/${accountIndex}'`,
+            network: networkType
+        })
 
-        // const publicKeyResult = await trezor.getPublicKey({
-        //     path: `m/44'/43'/${accountIndex}'`,
-        //     network: networkType
-        // })
-
-        const publicKeyResult = {
-            success: true,
-            payload: {
-                address: 'TAXQ2KXXMRZHPD7T2PF3KMXFW6RFW5RLOYLDMMHV', // my testnet wallet
-                path: [44, 43, 1, 0, accountIndex], // example path from from nemSignTransaction docs
-                // https://github.com/trezor/connect/blob/develop/docs/methods/nemSignTransaction.md
-                // https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
-                serializedPath: `m/44'/43'/1'/0/${accountIndex}`,
-                // xpub: string,        // xpub in legacy format
-                // xpubSegwit?: string, // optional for segwit accounts: xpub in segwit format
-                // chainCode: string,   // BIP32 serialization format
-                // childNum: number,    // BIP32 serialization format
-                publicKey: 'D783A98F4322B212FCBC3296918401F3B46979385393A953DC37339CA050D9B3',   // BIP32 serialization format
-                // fingerprint: number, // BIP32 serialization format
-                // depth: number,
-            }
-        }
-
-
-
-
-        // const accountResult = await trezor.nemGetAddress({
-        //     path: `m/44'/43'/${accountIndex}'`,
-        //     network: networkType
-        // })
-
-        // this is the shape of a successful device interaction
-        const accountResult = {
-            success: true,
-            payload: {
-                address: 'TAXQ2KXXMRZHPD7T2PF3KMXFW6RFW5RLOYLDMMHV', // my testnet wallet
-                path: [44, 43, 1, 0, accountIndex], // example path from from nemSignTransaction docs
-                // https://github.com/trezor/connect/blob/develop/docs/methods/nemSignTransaction.md
-                // https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
-                serializedPath: `m/44'/43'/1'/0/${accountIndex}`,
-            }
-        }
+        const accountResult = await trezor.nemGetAddress({
+            path: `m/44'/43'/${accountIndex}'`,
+            network: networkType
+        })
 
         // a successful result will contain
         console.log('RESULT', accountResult, publicKeyResult);
