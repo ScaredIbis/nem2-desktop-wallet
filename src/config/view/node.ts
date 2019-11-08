38bbab9869c07021526fec32f036c78ab06e60c1
decentraliser <decentraliser@gmail.com>
2019-11-08 09:52:19 +0800

    change default node

--- src/config/view/node.ts
+++ src/config/view/node.ts
@@ -5,7 +5,7 @@ export const nodeListConfig: Endpoint[] = [
         value: 'http://api-01.mt.ap-northeast-1.nemtech.network:3000',
         name: 'northeast',
         url: 'api-01.mt.ap-northeast-1.nemtech.network',
-        isSelected: true,
+        isSelected: false,
     },
     {
         value: 'http://api-01.mt.ap-southeast-1.nemtech.network:3000',
@@ -29,7 +29,7 @@ export const nodeListConfig: Endpoint[] = [
         value: 'http://api-01.mt.us-west-2.nemtech.network:3000',
         name: 'west',
         url: 'api-01.mt.us-west-2.nemtech.network',
-        isSelected: false,
+        isSelected: true,
     },
 
     // test net node
