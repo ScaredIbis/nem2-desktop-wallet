8ad8a9e5f214b66cf6d3ed64af0673842a372712
decentraliser <decentraliser@gmail.com>
2019-11-19 11:35:39 +0800

    save WIP on keystore dialogue

--- src/views/wallet/wallet-details/keystore-dialog/KeystoreDialogTs.ts
+++ src/views/wallet/wallet-details/keystore-dialog/KeystoreDialogTs.ts
@@ -1,18 +1,23 @@
+import {Component, Vue, Prop, Provide} from 'vue-property-decorator'
+import {mapState} from "vuex"
 import {Message} from "@/config/index.ts"
 import {copyTxt} from "@/core/utils"
-import {Component, Vue, Prop} from 'vue-property-decorator'
-import {mapState} from "vuex"
+import {validation} from '@/core/validation'
 import {AppWallet, StoreAccount} from "@/core/model"
+import ErrorTooltip from '@/components/other/forms/errorTooltip/ErrorTooltip.vue'
 
 @Component({
     computed: {
         ...mapState({
             activeAccount: 'account',
         })
-    }
+    },
+    components: {ErrorTooltip}
 })
 export class KeystoreDialogTs extends Vue {
+    @Provide() validator: any = this.$validator
     activeAccount: StoreAccount
+    validation = validation
     stepIndex = 0
     QRCode = ''
     keystoreText = ''
@@ -27,8 +32,8 @@ export class KeystoreDialogTs extends Vue {
         return this.showKeystoreDialog
     }
 
-    set show(val){
-        if(!val){
+    set show(val) {
+        if (!val) {
             this.$emit('closeKeystoreDialog')
         }
     }
@@ -37,15 +42,23 @@ export class KeystoreDialogTs extends Vue {
         return this.activeAccount.wallet
     }
 
+    checkWalletPassword() {
+        this.$validator
+            .validate()
+            .then((valid) => {
+                if (valid) this.stepIndex = 1
+            })
+    }
+
     exportKeystore() {
         switch (this.stepIndex) {
-            case 0 :
+            case 0:
                 this.checkWalletPassword()
                 break
-            case 1 :
+            case 1:
                 this.generateKeystore()
                 break
-            case 2 :
+            case 2:
                 this.stepIndex = 3
                 break
         }
@@ -53,43 +66,6 @@ export class KeystoreDialogTs extends Vue {
 
     async generateKeystore() {
         this.keystoreText = new AppWallet(this.getWallet).getKeystore()
-        this.stepIndex = 2
-    }
-
-    // @VEE-VALIDATE
-    checkWalletPassword() {
-        if (!this.checkInput()) return
-        this.stepIndex = 1
-    }
-
-    checkInput() {
-        if (!this.wallet.password || this.wallet.password == '') {
-            this.$Notice.error({
-                title: this.$t(Message.PLEASE_SET_WALLET_PASSWORD_INFO) + ''
-            })
-            return false
-        }
-
-        if (this.wallet.password.length < 8) {
-            this.$Notice.error({
-                title: this.$t(Message.WRONG_PASSWORD_ERROR) + ''
-            })
-            return false
-        }
-
-        const validPassword = new AppWallet(this.getWallet).checkPassword(this.wallet.password)
-
-        if (!validPassword) {
-            this.$Notice.error({
-                title: this.$t(Message.WRONG_PASSWORD_ERROR) + ''
-            })
-            return false
-        }
-
-        return true
-    }
-
-    toPrevPage() {
         this.stepIndex = 2
     }
 
