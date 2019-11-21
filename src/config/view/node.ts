66c490404773b8f8e078ff9e67ff1236153a4a35
decentraliser <decentraliser@gmail.com>
2019-11-21 14:22:52 +0800

    add testnet node

--- src/config/view/node.ts
+++ src/config/view/node.ts
@@ -2,6 +2,12 @@ import {Endpoint} from '@/core/model'
 
 export const nodeListConfig: Endpoint[] = [
     {
+        value: 'http://api-01.us-east-1.nemtech.network:3000',
+        name: 'newtn',
+        url: 'api-01.mt.ap-northeast-1.nemtech.network',
+        isSelected: true,
+    },
+    {
         value: 'http://api-01.mt.ap-northeast-1.nemtech.network:3000',
         name: 'northeast',
         url: 'api-01.mt.ap-northeast-1.nemtech.network',
@@ -11,7 +17,7 @@ export const nodeListConfig: Endpoint[] = [
         value: 'http://api-01.mt.ap-southeast-1.nemtech.network:3000',
         name: 'southeast',
         url: 'api-01.mt.ap-southeast-1.nemtech.network',
-        isSelected: true,
+        isSelected: false,
     },
     {
         value: 'http://api-01.mt.eu-central-1.nemtech.network:3000',
