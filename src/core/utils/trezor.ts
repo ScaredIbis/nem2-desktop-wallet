b6216bdc93ff260c4ae6047af0b70eb7994a3f93
Chris Martin <chris@crunchycloud.io>
2019-12-23 21:11:34 +1000

    install published version of trezor-connect

--- src/core/utils/trezor.ts
+++ src/core/utils/trezor.ts
@@ -3,11 +3,10 @@ import TrezorConnect from 'trezor-connect';
 // TODO: figure out who wants to be the first point of contact for trezor
 // https://github.com/trezor/connect/blob/develop/docs/index.md#trezor-connect-manifest
 TrezorConnect.init({
-    // email: 'chris@crunchycloud.io',
-    // appUrl: 'http://localhost:8080',
-    connectSrc: "http://localhost:8088/",
-    lazyLoad: true,
-    debug: true,
+    // ***Enable these fields when developing locally***
+    // connectSrc: "http://localhost:8088/",
+    // lazyLoad: true,
+    // debug: true,
     manifest: {
       email: 'chris@crunchycloud.io',
       appUrl: 'http://localhost:8080'
