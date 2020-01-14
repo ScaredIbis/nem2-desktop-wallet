3b3a91b804efffc242d9b55a282139c9ca53d9ad
Chris Martin <chris@crunchycloud.io>
2020-01-14 23:20:04 +1000

    update trezor manifest first point of contact

--- src/core/utils/trezor.ts
+++ src/core/utils/trezor.ts
@@ -3,14 +3,14 @@ import TrezorConnect from 'trezor-connect'
 // TODO: figure out who wants to be the first point of contact for trezor
 // https://github.com/trezor/connect/blob/develop/docs/index.md#trezor-connect-manifest
 TrezorConnect.init({
-    // ***Enable these fields when developing locally***
-    connectSrc: "http://localhost:8088/",
-    lazyLoad: true,
-    debug: true,
-    manifest: {
-      email: 'chris@crunchycloud.io',
-      appUrl: 'http://localhost:8080'
-    }
+  // ***Enable these fields when developing locally***
+  connectSrc: 'http://localhost:8088/',
+  lazyLoad: true,
+  debug: true,
+  manifest: {
+    email: 'devs@nem.foundation',
+    appUrl: 'http://localhost:8080',
+  },
 })
 
 export default TrezorConnect
