33c28c0820b0405f8ed52ef4da7a8f52d3e71b1d
Chris Martin <chris@crunchycloud.io>
2020-01-14 22:04:21 +1000

    add localization to account creation wizard

--- src/config/view/login.ts
+++ src/config/view/login.ts
@@ -4,19 +4,19 @@ import seedImg from "@/common/img/login/seed.png"
 import trezorImg from "@/common/img/login/trezor.png"
 import ledgerImg from "@/common/img/login/ledger.png"
 
-import importTrezorStepImage1 from '@/common/img/login/1_2.png'
-import importTrezorStepImage2 from '@/common/img/login/2_2.png'
+import importTrezorStepImage1 from "@/common/img/login/1_2.png"
+import importTrezorStepImage2 from "@/common/img/login/2_2.png"
 
-import importStepImage1 from '@/common/img/login/1_4.png'
-import importStepImage2 from '@/common/img/login/2_4.png'
-import importStepImage3 from '@/common/img/login/3_4.png'
-import importStepImage4 from '@/common/img/login/4_4.png'
+import importStepImage1 from "@/common/img/login/1_4.png"
+import importStepImage2 from "@/common/img/login/2_4.png"
+import importStepImage3 from "@/common/img/login/3_4.png"
+import importStepImage4 from "@/common/img/login/4_4.png"
 
-import createStepImage1 from '@/common/img/login/1_5.png'
-import createStepImage2 from '@/common/img/login/2_5.png'
-import createStepImage3 from '@/common/img/login/3_5.png'
-import createStepImage4 from '@/common/img/login/4_5.png'
-import createStepImage5 from '@/common/img/login/5_5.png'
+import createStepImage1 from "@/common/img/login/1_5.png"
+import createStepImage2 from "@/common/img/login/2_5.png"
+import createStepImage3 from "@/common/img/login/3_5.png"
+import createStepImage4 from "@/common/img/login/4_5.png"
+import createStepImage5 from "@/common/img/login/5_5.png"
 
 export const importInfoList = [
     {
@@ -40,23 +40,23 @@ export const importInfoList = [
 ]
 
 export const createStepBarTitleList = [
-    'Create Account',
-    'Generate Mnemonic',
-    'BackUp Mnemonic Phrase',
-    'Verify Mnemonic phrase',
-    'Finished'
+    "create_account_step_create_account",
+    "create_account_step_generate_mnemonic",
+    "create_account_step_backup_mnemonic_phrase",
+    "create_account_step_verify_mnemonic_phrase",
+    "create_account_step_finished"
 ]
 
 export const importStepBarTitleList = [
-    'Create Account',
-    'Type Mnemonic Phrase',
-    'Choose Wallets',
-    'Finished'
+    "import_account_step_create_account",
+    "import_account_step_type_mnemonic_phrase",
+    "import_account_step_choose_wallets",
+    "import_account_step_finished"
 ]
 
 export const importTrezorStepBarTitleList = [
-    'Create Account',
-    'Import Trezor Account'
+    "trezor_step_create_account",
+    "trezor_step_import_account"
 ]
 
 export const importStepImage = {
@@ -75,6 +75,6 @@ export const createStepImage = {
 }
 
 export const importTrezorStepImage = {
-    importTrezorStepImage1,      
+    importTrezorStepImage1,
     importTrezorStepImage2,
 }
